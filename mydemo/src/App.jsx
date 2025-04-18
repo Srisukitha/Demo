d36d31a2695d14287054d802b0867749d3adb03a
Srisukitha <sukithasuresh168@gmail.com>
2025-04-18 10:05:50 +0530

    I imported a Box1,Box2,Box3 file

--- mydemo/src/App.jsx
+++ mydemo/src/App.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import StyleContext from './StyleContext';
-
+import Box1 from './Box1';
+import Box2 from './Box2';
+import Box3 from './Box3';
 import Todo from './Todo'; 
 
 function App() {
@@ -14,7 +16,9 @@ function App() {
   return (
     <StyleContext.Provider value={{ styles, setStyles }}>
       <div style={{ ...styles.page, height: '100vh', display: 'flex', gap: '20px', justifyContent: 'center', alignItems: 'center' }}>
-       
+        <Box1 />
+        <Box2 />
+        <Box3 />
         <Todo />
       </div>
     </StyleContext.Provider>
